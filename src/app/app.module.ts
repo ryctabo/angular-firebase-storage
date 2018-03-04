4e53bc308a6536fe6d9afa0fe15b0279967f0b55
Gustavo Pacheco <ryctabo@gmail.com>
2018-03-04 17:25:18 -0500

    chore:Configure Angular Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatButtonModule, MatProgressBarModule } from '@angular/material';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireStorageModule } from 'angularfire2/storage';
 
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -12,9 +15,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+
+    /** Material Angular Modules */
     BrowserAnimationsModule,
     MatButtonModule,
-    MatProgressBarModule
+    MatProgressBarModule,
+
+    /** Angular Firebase Modules */
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireStorageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAS5ik3hEFQ8fmBtAaSN5MzXwtnf8P1L9s',
+    authDomain: 'angular-firebase-blog-c0906.firebaseapp.com',
+    databaseURL: 'https://angular-firebase-blog-c0906.firebaseio.com',
+    projectId: 'angular-firebase-blog-c0906',
+    storageBucket: '',
+    messagingSenderId: '32411828157'
+  }
 };
